2e6d74fe491b64c5fb0cec31735436cd6f19fb33
Chinwen Chou <ccw.cm91@gmail.com>
2026-04-16 00:54:12 +0800

    set ffmpeg path, add debug log

--- mp3totxt.py
+++ mp3totxt.py
@@ -1,7 +1,15 @@
+print("🔥 程式有執行")
+
+import os
 import whisper
 
-model = whisper.load_model("small")  # tiny / base / small / medium / large
+os.environ["PATH"] += os.pathsep + r"D:\tools_exe\ffmpeg-7.0.1-full_build\bin"
 
-result = model.transcribe("audio.mp3")
+model = whisper.load_model("small") # tiny / base / small / medium / large
 
+print("🚀 開始轉錄...")
+
+result = model.transcribe("audio.mp3", verbose=True)
+
+print("✅ 完成")
 print(result["text"])
